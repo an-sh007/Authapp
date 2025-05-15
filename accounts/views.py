d599b52fa40476e85f760eff427973ba096f8ab4
an-sh007 <102963297+an-sh007@users.noreply.github.com>
2025-05-15 14:33:19 +0530

    new

--- accounts/views.py
+++ accounts/views.py
@@ -1,4 +1,14 @@
 from django.http import HttpResponse
-
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
 def home(request):
     return HttpResponse("Welcome to AuthApp!")
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)  # Grab the data from form submission
+        if form.is_valid():
+            form.save()  # Save the new user
+            return redirect('login')  # After registration, send user to login page (we’ll create it soon)
+    else:
+        form = UserCreationForm()  # Show an empty registration form if GET request
+    return render(request, 'accounts/register.html', {'form': form})
